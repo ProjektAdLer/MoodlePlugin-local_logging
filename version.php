57d5b7db6d7ecddf567fa8783751ac78fa985c49
Markus Heck <markus.heck@hs-kempten.de>
2024-06-03 16:47:59 +0200

    update version.php for release 0.2.0

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_logging'; // Full name of the plugin.
-$plugin->version = 2024011704;
+$plugin->version = 2024060300;
 $plugin->requires  = 2022112800;      // Requires this Moodle version.
 $plugin->maturity  = MATURITY_STABLE; // Maturity level of the plugin.
-$plugin->release   = '0.1.0';          // Human-readable version name.
+$plugin->release   = '0.2.0';          // Human-readable version name.
