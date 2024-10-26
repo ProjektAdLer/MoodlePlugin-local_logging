9efd4697be102d9bd836f18744e81423ef79f779
Markus <markus.heck@hs-kempten.de>
2024-10-26 13:14:51 +0200

    Fix wrong min moodle version

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_logging'; // Full name of the plugin.
-$plugin->version = 2024060300;
+$plugin->version = 2024091200;
 $plugin->requires  = 2022112800;      // Requires this Moodle version.
 $plugin->maturity  = MATURITY_STABLE; // Maturity level of the plugin.
 $plugin->release   = '0.2.0';          // Human-readable version name.
