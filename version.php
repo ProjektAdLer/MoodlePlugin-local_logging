f6bd5d6a6ba4c0bf4f3731dfbac3d8003b35f6b3
Markus <markus.heck@hs-kempten.de>
2025-03-17 01:38:24 +0100

    bump version.php

--- version.php
+++ version.php
@@ -2,8 +2,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025030300;
+$plugin->version = 2025031600;
 $plugin->requires  = 2024042200;      // Requires this Moodle version.
 $plugin->component = 'local_logging'; // Full name of the plugin.
-$plugin->release   = '1.0.0-dev';          // Human-readable version name.
-$plugin->maturity  = MATURITY_ALPHA; // Maturity level of the plugin.
+$plugin->release   = '2.1.0';          // Human-readable version name.
+$plugin->maturity  = MATURITY_STABLE; // Maturity level of the plugin.
