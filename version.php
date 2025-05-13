62e0e7ab901dbbe651305c7ad0c098b4c602865f
Markus Heck <51326311+Glutamat42@users.noreply.github.com>
2025-05-13 14:59:56 +0200

    bump version for release

--- version.php
+++ version.php
@@ -2,8 +2,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025031600;
+$plugin->version = 2025051300;
 $plugin->requires  = 2024042200;      // Requires this Moodle version.
 $plugin->component = 'local_logging'; // Full name of the plugin.
-$plugin->release   = '2.1.0';          // Human-readable version name.
+$plugin->release   = '2.2.0';          // Human-readable version name.
 $plugin->maturity  = MATURITY_STABLE; // Maturity level of the plugin.
